adc89dff86588fb600f31a15fbdd6741929e710f
alu0100821390 <alu0100821390@ull.edu.es>
2017-04-13 17:02:45 +0100

    Corregir fecha

--- rc4.py
+++ rc4.py
@@ -3,7 +3,7 @@
 ## Escuela Superior de Ingeniería y Tecnología	 			    ##
 ## Grado en Ingeniería Informática				 	    ##
 ## Seguridad en Sistemas Informáticos			 		    ##
-## Fecha: 07/03/2016							    ##
+## Fecha: 07/03/2017							    ##
 ## Autor: Kevin Estévez Expósito (alu0100821390) 			    ##
 ## 									    ##
 ## Práctica 3: Cifrado RC4						    ##
